e2a1bc51178d0104ac72daf0303da0f34495589e
Sergey <deliozzz@gmail.com>
2019-02-18 17:27:51 +0300

    more work

--- MyFriends/app/src/main/java/xyz/dudedaya/myfriends/DataHelper.java
+++ MyFriends/app/src/main/java/xyz/dudedaya/myfriends/DataHelper.java
@@ -1,24 +1,33 @@
 package xyz.dudedaya.myfriends;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DataHelper {
 
-    private String token;
-    private FriendsDatabaseHelper databaseHelper;
+    public interface DataEventListener {
+        void dataLoaded();
+        void errorLoadingData();
+    }
 
-    DataHelper(String token, Context context) {
+    private String token;
+    private List<User> users;
+    private DataEventListener listener;
+
+    DataHelper(String token, DataEventListener listener) {
         this.token = token;
-        databaseHelper = new FriendsDatabaseHelper(context);
+        this.listener = listener;
     }
 
     public void loadData() {
@@ -32,7 +41,7 @@ public class DataHelper {
         protected String doInBackground(Void... voids) {
             StringBuilder response = new StringBuilder();
             try {
-                URL request = new URL("https://api.vk.com/method/users.get?fields=nickname,photo_100&v=5.89&token=" + token);
+                URL request = new URL("https://api.vk.com/method/users.get?fields=nickname,photo_100&v=5.89&access_token=" + token);
                 HttpURLConnection urlConnection = (HttpURLConnection) request.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -46,7 +55,7 @@ public class DataHelper {
                 } finally {
                     urlConnection.disconnect();
                 }
-                request = new URL("https://api.vk.com/method/friends.get?fields=nickname,photo_100,status&order=random&count=5&v=5.8&token=" + token);
+                request = new URL("https://api.vk.com/method/friends.get?fields=nickname,photo_100,status&order=random&count=5&v=5.8&access_token=" + token);
                 urlConnection = (HttpURLConnection) request.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -69,31 +78,74 @@ public class DataHelper {
         @Override
         protected void onPostExecute(String s) {
             if (s == null) {
-                // todo: error
+                listener.errorLoadingData();
             } else {
-                new WriteDataTask().execute(s);
+                String testString = "{\"response\":[{\"id\":297428682,\"first_name\":\"Jared\",\"last_name\":\"Leto\",\"nickname\":\"Letovan\",\"photo_100\":\"someurl\"}]} " +
+                        "{\"response\":[{\"id\":210700287,\"first_name\":\"first_name1\",\"last_name\":\"last_name1\"}," +
+                        "{\"id\":210700287,\"first_name\":\"first_name2\",\"last_name\":\"last_name2\"}," +
+                        "{\"id\":210700287,\"first_name\":\"first_name3\",\"last_name\":\"last_name3\"}," +
+                        "{\"id\":210700287,\"first_name\":\"first_name4\",\"last_name\":\"last_name4\"}," +
+                        "{\"id\":210700287,\"first_name\":\"first_name5\",\"last_name\":\"last_name5\"}]}";
+                Pattern pattern = Pattern.compile("(\\{\"id\":[^\\}]*\\})");
+                Matcher matcher = pattern.matcher(testString);
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                users = new ArrayList<>();
+                while (matcher.find()) {
+                    User user = gson.fromJson(matcher.group(), User.class);
+                    users.add(user);
+                }
+                listener.dataLoaded();
             }
         }
     }
 
-    private class WriteDataTask extends AsyncTask<String , Void, String> {
+    public class User {
+        public String getName() {
+            if (nickname != null && !nickname.equals("") && !nickname.equals("null")) {
+                return first_name + " " + nickname + " " + last_name;
+            } else return first_name + " " + last_name;
+        }
+
+        public String getPhoto_100() {
+            return photo_100;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        private String first_name;
+        private String last_name;
+        private String nickname;
+        private String photo_100;
+        private String status;
+
+        public User(String first_name, String last_name, String nickname, String photo_100, String status) {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.nickname = nickname;
+            this.photo_100 = photo_100;
+            this.status = status;
+        }
 
         @Override
-        protected String doInBackground(String... stringData) {
-            ArrayList<String[]> friendsData = parceFriends(stringData[0]);
-            try (SQLiteDatabase db = databaseHelper.getWritableDatabase()) {
-                for (int i = 1; i < 6; i++) {
-
-                }
-
-            }
-            return null;
+        public String toString() {
+            return "first_name: " + first_name + " last_name: " + last_name + " nickname: " + nickname + " photo_100: " + photo_100 + " status: " + status;
         }
     }
 
-    private HashMap<String, Object> parceFriends(String friendsDataString) {
-        ArrayList<String[]> friendsData = new ArrayList<>();
-        //todo: parce
-        return friendsData;
+    public User getUser() {
+        if (users.size() > 0) {
+            return users.get(0);
+        } else return null;
+    }
+
+    public List<User> getFriends() {
+        if (users.size() > 1) {
+            List<User> friends = users;
+            friends.remove(0);
+            return friends;
+        } else return null;
     }
 }
--- MyFriends/app/src/main/java/xyz/dudedaya/myfriends/MainActivity.java
+++ MyFriends/app/src/main/java/xyz/dudedaya/myfriends/MainActivity.java
@@ -2,10 +2,8 @@ package xyz.dudedaya.myfriends;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,16 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity implements LoginFragment.AuthClickListener, AuthFragment.AuthListener {
+public class MainActivity extends AppCompatActivity implements LoginFragment.AuthClickListener, AuthFragment.AuthListener, DataHelper.DataEventListener {
 
     private SharedPreferences sharedPreferences;
     private String token;
@@ -57,40 +47,65 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Aut
         }
     }
 
+
     private boolean checkToken() {
         long currentTime = System.currentTimeMillis();
         token = sharedPreferences.getString("token", "");
+        Log.d("checkToken",token);
         long expirationDate = sharedPreferences.getLong("expirationDate", currentTime);
         // Return false if we don't have a token or the token is expired
         return (token.length() != 0 && expirationDate >= currentTime);
     }
 
+    private void saveToken() {
+        long currentTime = System.currentTimeMillis() + 86400 * 1000;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("token", token);
+        editor.putLong("expirationDate", currentTime);
+        editor.apply();
+    }
+
+    private void resetToken() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+
     private void setUI(boolean isTokenOk) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         if (isTokenOk) {
-            // Set Friends fragment
-            FriendsFragment friendsFragment = new FriendsFragment();
-            fragment = friendsFragment;
-            ft.add(R.id.fragment_container, friendsFragment);
+            retrieveData();
         } else {
             // Set Authorize fragment
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             LoginFragment loginFragment = new LoginFragment();
             fragment = loginFragment;
             ft.add(R.id.fragment_container, loginFragment);
+            ft.commit();
         }
-        ft.commit();
     }
 
 
-    public void buttonClick(View view) {
-        // todo: remove
-        logoutMenuItem.setVisible(!logoutMenuItem.isVisible());
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (fragment.isAdded()) {
+            getSupportFragmentManager().putFragment(outState, "fragment", fragment);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         logoutMenuItem = menu.findItem(R.id.action_logout);
+        logoutMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                resetToken();
+                logoutMenuItem.setVisible(false);
+                setUI(false);
+                return true;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -108,15 +123,42 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Aut
 
     @Override
     public void authComplete(String url) {
-        fragment = new FriendsFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, fragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+        token = url.split("access_token=")[1].split("&")[0];
+        if (token.length() > 0) {
+            saveToken();
+            retrieveData();
+        } else showErrorSnackbar();
     }
 
     @Override
     public void authError() {
+        showErrorSnackbar();
+    }
+
+    @Override
+    public void authLoaded() {
+        hideProgressBar();
+    }
+
+    @Override
+    public void dataLoaded() {
+        Log.d("dataLoaded", "dataLoaded");
+        setFriendsFragment();
+        showLogoutButton();
+    }
+
+    @Override
+    public void errorLoadingData() {
+        showErrorSnackbar();
+    }
+
+    private void retrieveData() {
+        Log.d("retrieveData", "retrieveData");
+        DataHelper dh = new DataHelper(token, this);
+        dh.loadData();
+    }
+
+    private void showErrorSnackbar() {
         Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), R.string.auth_error, Snackbar.LENGTH_LONG);
         snackbar.setAction(R.string.snackbar_retry, new View.OnClickListener() {
             @Override
@@ -135,11 +177,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Aut
             ft.replace(R.id.fragment_container, fragment);
             ft.commit();
         }
-    }
 
-    @Override
-    public void authLoaded() {
-        hideProgressBar();
     }
 
     private void showProgressBar() {
@@ -150,6 +188,11 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Aut
         progressBar.setVisibility(View.GONE);
     }
 
+    private void showLogoutButton() {
+        Log.d("showLogoutButton", "logoutShow");
+        logoutMenuItem.setVisible(true);
+    }
+
     private void setFriendsFragment() {
         fragment = new FriendsFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -158,12 +201,5 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Aut
         ft.commit();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (fragment.isAdded()) {
-            getSupportFragmentManager().putFragment(outState, "fragment", fragment);
-        }
-    }
 
 }
