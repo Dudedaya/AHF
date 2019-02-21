2d05c3ea5ad650d7fed040ac5e2c4832c6640bc7
Sergey <deliozzz@gmail.com>
2019-02-21 16:30:27 +0300

    Path, Query, QueryMap, Url annotation examples
    Added model class Comment, Changed JsonPlaceHolderApi and MainActivity classes to illustrate more retrofit annotations.

--- RetrofitExample/app/src/main/java/xyz/dudedaya/retrofitexample/MainActivity.java
+++ RetrofitExample/app/src/main/java/xyz/dudedaya/retrofitexample/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,7 +15,13 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private int postId = 3;
+    private Integer userId[] = {1, 4};
+    private String sort = "id";
+    private String order = "desc";
+
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +34,23 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        getPosts(userId, sort, order);
+        //getComments(postId);
+
+    }
+
+    private void getPosts(Integer userIds[], String sort, String order) {
+//        To pass unspecified parameters we using Map<String, String>
+//        Map<String, String> parameters = new HashMap<>();
+//        parameters.put("userId", "1"); //But with this approach we can only pass 1 of each query
+//        parameters.put("_sort", "id");
+//        parameters.put("_order", "desc");
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
+
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(userIds, sort, order); // can be set to null if it doesn't used
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -57,5 +79,45 @@ public class MainActivity extends AppCompatActivity {
                 textViewResult.setText(t.getMessage());
             }
         });
+    }
+
+    private void getComments(int postId) {
+//        We can pass the whole url or the endpoint here too
+//        String ulr = "https://jsonplaceholder.typicode.com/posts?userId=2&_sort=id&_order=desc"
+//        --or--
+//        String url = "posts?userId=2&_sort=id&_order=desc";
+//        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(url);
+
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(postId);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                if (!response.isSuccessful()) {
+                    String responseCode = "Code " + response.code();
+                    textViewResult.setText(responseCode);
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for (Comment comment : comments) {
+                    StringBuilder content = new StringBuilder();
+                    content.append("ID: ").append(comment.getId()).append("\n");
+                    content.append("Post ID: ").append(comment.getPostId()).append("\n");
+                    content.append("Name : ").append(comment.getName()).append("\n");
+                    content.append("Email : ").append(comment.getEmail()).append("\n");
+                    content.append("Text : ").append(comment.getText()).append("\n\n");
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
     }
 }
