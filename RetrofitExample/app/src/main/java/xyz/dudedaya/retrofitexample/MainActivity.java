961198dac04d974001b252938bbaa5dba5e26772
Sergey <deliozzz@gmail.com>
2019-02-21 14:33:49 +0300

    Initial commit
    Retrofit example app. An example how to use retrofit with a fake GSON API (jsonplaceholder.com/posts).

---
+++ RetrofitExample/app/src/main/java/xyz/dudedaya/retrofitexample/MainActivity.java
@@ -0,0 +1,61 @@
+package xyz.dudedaya.retrofitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    String responseCode = "Code " + response.code();
+                    textViewResult.setText(responseCode);
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                for (Post post : posts) {
+                    StringBuilder content = new StringBuilder();
+                    content.append("ID: ").append(post.getId()).append("\n");
+                    content.append("User ID: ").append(post.getUserId()).append("\n");
+                    content.append("Title : ").append(post.getTitle()).append("\n");
+                    content.append("Text : ").append(post.getText()).append("\n\n");
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
